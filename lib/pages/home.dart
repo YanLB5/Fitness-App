5207d21fb56b541ada9e887c48d1298514c11a3f
Yan Lima Barbosa <yanbr763@gmail.com>
2023-06-18 05:24:22 -0300

    search field

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: sort_child_properties_last, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -7,27 +9,99 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Breakfast',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
+        appBar: appBar(),
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                    color: Color(0xff1D1617).withOpacity(0.11),
+                    blurRadius: 40,
+                    spreadRadius: 0.0)
+              ]),
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  contentPadding: EdgeInsets.all(15),
+                  hintText: 'Search Pizza',
+                  hintStyle: TextStyle(
+                    color: Color(0xffDDDADA),
+                    fontSize: 14
+                  ),
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: SvgPicture.asset("assets/icons/search.svg"),
+                  ),
+                  suffixIcon: Container(
+                    width: 100,
+                    child: IntrinsicHeight(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          VerticalDivider(
+                            color: Colors.black,
+                            indent: 10,
+                            endIndent: 10,
+                            thickness: 0.1,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: SvgPicture.asset("assets/icons/filter.svg"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide.none)
+                ),
+              ),
+            )
+          ],
+        ));
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text(
+        'Breakfast',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      backgroundColor: Colors.white,
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          child: SvgPicture.asset(
+            'assets/icons/left-arrow.svg',
+            height: 20,
+            width: 20,
+          ),
+          decoration: BoxDecoration(
+            color: Color(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10),
           ),
         ),
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        leading: GestureDetector(
-          onTap: () {
-
-            
-          },
+      ),
+      actions: [
+        GestureDetector(
+          onTap: () {},
           child: Container(
             margin: EdgeInsets.all(10),
             alignment: Alignment.center,
+            width: 37,
             child: SvgPicture.asset(
-              'assets/icons/left-arrow.svg',
+              'assets/icons/two-dots.svg',
               height: 20,
               width: 20,
             ),
@@ -37,29 +111,7 @@ class HomePage extends StatelessWidget {
             ),
           ),
         ),
-        actions: [
-          GestureDetector(
-            onTap: () {
-
-            
-            },
-            child: Container(
-              margin: EdgeInsets.all(10),
-              alignment: Alignment.center,
-              width: 37,
-              child: SvgPicture.asset(
-                'assets/icons/two-dots.svg',
-                height: 20,
-                width: 20,
-              ),
-              decoration: BoxDecoration(
-                color: Color(0xffF7F8F8),
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
-          ),
-        ],
-      ),
+      ],
     );
   }
 }

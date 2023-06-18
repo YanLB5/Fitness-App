4255f8985b6989bb92bc9f18d38a38ea81e0c0c4
Yan Lima Barbosa <yanbr763@gmail.com>
2023-06-18 12:39:38 -0300

    diet model

---
+++ lib/models/diet_model.dart
@@ -0,0 +1,32 @@
+class DietModel {
+  String name;
+  String iconPath;
+  String level;
+  String duration;
+  String calorie;
+  bool viewIsSelected;
+
+  DietModel({
+    required this.name,
+    required this.iconPath,
+    required this.level,
+    required this.duration,
+    required this.calorie,
+    required this.viewIsSelected
+  });
+
+  static List < DietModel > getDiets() {
+    List < DietModel > diets = [];
+
+    diets.add(
+      DietModel(
+        name: "Honey Pancake",
+        iconPath: "assets/icons/pancakes-dessert.svg",
+        level: "Easy",
+        duration: "30mins",
+        calorie: "180kCal",
+        viewIsSelected: true
+      )
+    );
+  }
+}
